4ed44124c284a55be5e564e1b09c9084bb9cbd66
Ameer Binmahfooz <84015032+amirmuallim@users.noreply.github.com>
2026-09-16 15:29:07 +0530

    Add comments to input signals in register_bank module
    Added comments to input signals for clarity.

--- rtl/register_bank.v
+++ rtl/register_bank.v
@@ -4,9 +4,9 @@ module register_bank(
 	input clk,
 	input signed [7:0] reg_in,
 	input write_reg,
-	input [3:0] k,
-	input [3:0] i,
-	input [3:0] j,
+	input [3:0] k, // write index
+	input [3:0] i, // read index 1 / left
+	input [3:0] j, // read index 2 / right
 	output signed [7:0] left_out,
 	output signed [7:0] right_out);
 
