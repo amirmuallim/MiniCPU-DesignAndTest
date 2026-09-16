e1eb8f2fc325eb58badca9d674e4a96dca15f1e2
Ameer Binmahfooz <84015032+amirmuallim@users.noreply.github.com>
2026-09-16 15:31:29 +0530

    Clean up commented code in register_bank.v
    Removed commented-out assignments for left_out and right_out.

--- rtl/register_bank.v
+++ rtl/register_bank.v
@@ -8,7 +8,8 @@ module register_bank(
 	input [3:0] i, // read index 1 / left
 	input [3:0] j, // read index 2 / right
 	output signed [7:0] left_out,
-	output signed [7:0] right_out);
+	output signed [7:0] right_out
+);
 
 reg signed [7:0] mem [15:0];
 
@@ -16,10 +17,6 @@ always @(posedge clk)
 begin
 	if(write_reg)
 		mem[k] <= reg_in;
-
-	// left_out <= mem[i];		
-	// right_out <= mem[j];
-	
 end
 
 assign left_out = mem[i];		
